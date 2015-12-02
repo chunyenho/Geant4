4d346e9d1061999652cf4c9f2ec6de335c5fcd0b
j <chunyenher@gmail.com>
2015-12-02 16:00:27 +0800

    first commit

---
+++ source/event/include/add_CUDAtag.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo class start
+class_start=`sed -n '/^class[ ].*[^;]$/=' $1`
+echo $class_start
+echo class end
+class_end=`sed -n '/^}\;/=' $1`
+echo $class_end
+
+if [[ -z "$class_start" ]];
+then
+echo "--------------------------$1--------------------------" >> exception_log.txt
+echo "$1 doesn't has a class." >> exception_log.txt    
+else
+sed -re "/^[ \t]*\/\/.*/! { /^[ \t]*(if|else|for|case|while|goto|sizeof|return|\{)./! { /^.* new [A-Za-z0-9_~=!&]+( *)\(.*\).*$/! {$class_start,$class_end s/^.* [A-Za-z0-9_~=!&]+( *)\(.*\).*$/__device__ __host__ &/gm}}}" $1
+
+fi
+#sed -re "/^[ \t]*\/\/.*/! {$class_start,$class_end s/^.* [A-Za-z0-9_~]*\(.*\).*$/__device__ __host__ &/gm}" $1
